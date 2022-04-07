5db16877bef2e900971936454f95777e420b3f1f
benjadiazu <>
2022-04-07 15:41:27 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -53,7 +53,7 @@ void enlarge(HashMap * map) {
 
 HashMap * createMap(long capacity) {
     HashMap *Mapa = (HashMap*)malloc(sizeof(HashMap));
-    Pair **Par = (Pair**)calloc(sizeof(Pair*),capacity);
+    Pair **Par = (Pair**)calloc(sizeof(Pair),capacity);
     Mapa->buckets = Par;
     Mapa->capacity = capacity;
     Mapa->current = -1;
