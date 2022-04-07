0a08b0fd836c6ca59a0b697ea2761b04d6152f6c
benjadiazu <>
2022-04-07 15:52:48 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -40,7 +40,7 @@ int is_equal(void* key1, void* key2){
 
 
 void insertMap(HashMap * map, char * key, void * value) {
-    int posicion =posicion = hash(key,map->capacity);
+    long posicion =posicion = hash(key,map->capacity);
 
     while (map->buckets[posicion] != NULL){
         posicion = hash(key,map->capacity);
