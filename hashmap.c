2f590480350bd7eb9abe8b0c944cf24dbfc779dc
benjadiazu <>
2022-04-07 15:30:07 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -56,6 +56,7 @@ HashMap * createMap(long capacity) {
     Mapa->capacity = capacity;
 
     Mapa->buckets = (Pair**)malloc(sizeof(Pair));
+    Mapa->buckets = NULL;
     Mapa->current = -1;
     
     return Mapa;
