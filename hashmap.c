4f9560fafdbb1a11f410fff5e41921533ba950ec
benjadiazu <>
2022-04-09 23:50:59 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -106,7 +106,9 @@ Pair * firstMap(HashMap * map) {
 Pair * nextMap(HashMap * map) {
     long posicion = (map->current) + 1;
 
-   
+    while (map->buckets[posicion] == NULL || map->buckets[posicion]->key == NULL){
+        posicion = (posicion+1)%(map->capacity);
+    }
     map->current = posicion;
     return map->buckets[posicion];
 }
