8db9f311d5f3bb07d806086fb584ffbd3e224006
benjadiazu <>
2022-04-10 18:01:12 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -57,14 +57,11 @@ void enlarge(HashMap * map) {
     enlarge_called = 1; //no borrar (testing purposes)
 
   
-    Pair **Aux = (Pair**)calloc(sizeof(Pair*),map->capacity);
-    Aux = map->buckets;
+    Pair **Aux = map->buckets;
     map->capacity *= 2;
     map->buckets = (Pair**)calloc(sizeof(Pair*),map->capacity);
     map->size = 0;
-    long posicion = 0;
-    insertMap(map,Aux[posicion]->key, Aux[posicion]->value);
-    
+
 
     
   
