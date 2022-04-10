525349929f15e9d9f58499785f2253bd27bdcb5a
benjadiazu <>
2022-04-10 00:07:27 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -108,7 +108,8 @@ Pair * nextMap(HashMap * map) {
     int i = map->capacity;
     while (map->buckets[posicion] == NULL || map->buckets[posicion]->key == NULL){
         posicion = (posicion+1)%(map->capacity);
-        
+         
+    }
     map->current = posicion;
     return map->buckets[posicion]; 
     
